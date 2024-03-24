6b09e48b7633838ea93c254d24fbe197692649be
Mike Wittenauer <mikewittenauer@me.com>
2024-03-24 12:31:46 -0400

    Hookup front end to back end and install concurrently for development

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
+
 function App() {
-  return (
-    <div className="App">
-        <h1>Hangman</h1>
-    </div>
-  );
+    const [game, setGame] = useState({category: '', solution: ''});
+    useEffect(() => {
+        const fetchGame = async () => {
+            try {
+                const response = await fetch(process.env.REACT_APP_API_URL, {
+                    method: 'GET'
+                });
+
+                const data = await response.json();
+
+                setGame(data);
+            } catch (error) {
+                console.error('Error fetching game', error.message);
+            }
+        }
+        fetchGame();
+    }, []);
+    return (
+        <div className="App">
+            <h1>Hangman</h1>
+        </div>
+    );
 }
 
 export default App;
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const hangmanWords = require('./hangmanWords');
+const cors = require('cors');
 const express = require('express');
 
 const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => {
     let solution = hangmanWords[Math.floor(Math.random()*hangmanWords.length)];
     res.json(solution);
 });
 
-app.listen(3000, () => {
-    console.log('API listening on port 3000');
+app.listen(3002, () => {
+    console.log('API listening on port 5000');
 });
